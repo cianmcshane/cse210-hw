6bcc1e859c1fb877668da719acdb35043f8c80d9
cianmcshane <cianmac18@byui.edu>
2026-04-25 21:59:59 -0600

    prep assignments 1 and 2 completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What's your first name?");
+        string cmfirstName = Console.ReadLine();
+        Console.WriteLine("What's your last name?");
+        string cmlastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {cmlastName}, {cmfirstName} {cmlastName}");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What was your grade percentage?");
+        string stringNumber = Console.ReadLine();
+        int cmgradeNumber = int.Parse(stringNumber);
+        string cmGrade = "";
+
+        if (cmgradeNumber >= 90)
+        {
+            cmGrade = "A";
+        }
+        else if (cmgradeNumber >= 80)
+        {
+            cmGrade = "B";
+        }
+        else if (cmgradeNumber >= 70)
+        {
+            cmGrade = "C";
+        }
+        else if (cmgradeNumber >= 60)
+        {
+            cmGrade = "D";
+        }
+        else
+        {
+            cmGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is a {cmGrade}");
     }
 }
