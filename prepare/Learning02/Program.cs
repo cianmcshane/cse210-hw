e770560d31561e3485e8a00a1eadfde1dd600e4e
cianmcshane <cianmac18@byui.edu>
2026-05-05 12:52:52 -0600

    Update Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,24 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job job1 = new Job();
-        job1._cmjobTitle = "Software Engineer";
-        job1._cmcompany = "Microsoft";
-        job1._cmstartYear = "2019";
-        job1._cmendYear = "2022";
+        Job cmjob1 = new Job();
+        cmjob1._cmjobTitle = "Software Engineer";
+        cmjob1._cmcompany = "Microsoft";
+        cmjob1._cmstartYear = "2019";
+        cmjob1._cmendYear = "2022";
 
-        Job job2 = new Job();
-        job2._cmjobTitle = "Manager";
-        job2._cmcompany = "Apple";
-        job2._cmstartYear = "2022";
-        job2._cmendYear = "2023";
+        Job cmjob2 = new Job();
+        cmjob2._cmjobTitle = "Manager";
+        cmjob2._cmcompany = "Apple";
+        cmjob2._cmstartYear = "2022";
+        cmjob2._cmendYear = "2023";
 
-        Resume myResume = new Resume();
-        myResume._cmname = "Allison Rose";
+        Resume cmmyResume = new Resume();
+        cmmyResume._cmname = "Allison Rose";
 
-        myResume._cmjobs.Add(job1);
-        myResume._cmjobs.Add(job2);
+        cmmyResume._cmjobs.Add(job1);
+        cmmyResume._cmjobs.Add(job2);
 
-        myResume.Display();
+        cmmyResume.Display();
     }
 }
