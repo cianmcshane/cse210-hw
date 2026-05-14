8395549deedad5b5b3a659c6dcea17bf58acf438
cianmcshane <cianmac18@byui.edu>
2026-05-14 13:27:23 -0600

    Update Program.cs

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,26 @@
-using System;
+using System.IO;
 
-class Program
-{
-    static void Main(string[] args)
+static void writeJoke()
+{string fileName = "myFile.txt";
+
+using (StreamWriter outputFile = new StreamWriter(fileName))
     {
-        Console.WriteLine("Hello Sandbox World!");
+        outputFile.WriteLine("Joke File");
+        outputFile.WriteLine("Why did the chicken cross the road?");
+        outputFile.WriteLine("To get to the other side hahahaha!");
+    }}
+
+
+static void readJoke()
+{
+    string filename = "myFile.txt";
+    string[] lines= System.IO.File.ReadAllLines(filename);
+
+    foreach (string line in lines)
+    {
+        Console.WriteLine(line);
     }
 }
+
+writeJoke();
+readJoke();
